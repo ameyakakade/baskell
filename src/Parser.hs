415ff82a7755340c9a3722592816016c75490de8
ameyakakade <ameyakakade@gmail.com>
2026-05-19 12:41:48 +0530

    Major work on Parser
    Parser stores a list of errors. The input holds last parsed line and
    column number, which makes it easier to report errors. Parser type is
    now a functor, applicative and alternative. The either type used for
    errors is also a alternative. If all the eithers are for errors, it
    picks the one which has parsed the farthest. There is a newErr
    function that adds errors to the error list. There is a parser for
    strings which is just sequenced char parsers.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,5 +1,11 @@
 module Parser where
 
+import Data.Char
+import Control.Applicative
+
+--           line  col  input
+type Input = (Int, Int, String)
+
 data BProgram = Program [BDefinition]
               deriving (Eq, Show)
 
@@ -20,7 +26,7 @@ data BStatement = Auto     [(BName, BConstant)]
                 | Switch   (BRValue, BStatement)
                 | Goto     BRValue
                 | Return   BRValue
-                | SRValue BRValue
+                | SRValue  BRValue
                 deriving (Eq, Show)
 
 data BRValue = BracketRValue BRValue
@@ -59,7 +65,7 @@ data BBinary = Or
              | Modulo
              | Multiply
              | Divide
-            deriving (Eq, Show)
+             deriving (Eq, Show)
 
 data BLValue = LName       BName
              | Dereference BRValue
@@ -72,5 +78,49 @@ data BConstant = Digit Int
 
 data BName = Name String
            deriving (Eq, Show)
+--                                                         Errors   input 
+newtype Parser a = Parser { runParser :: Input -> Either ([String], Input) (a, Input)}
 
-newtype Parser a = Parser { runParser :: String -> Either (Int, Int, String) (String, a)}
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input -> do
+    (a, restIn) <- p input
+    return (f a, restIn)
+
+instance Applicative Parser where
+  pure x = Parser $ \input -> Right (x, input) 
+  (<*>) (Parser f) (Parser p) = Parser $ \input -> do
+    (f, input') <- f input
+    (a, input'') <- p input'
+    return (f a, input'')
+
+instance Alternative Parser where
+  empty = Parser $ \input -> Left (["Failed parser."], input)
+  (Parser p1) <|> (Parser p2) = Parser $ \input -> p1 input <|> p2 input
+
+instance Alternative (Either ([String], Input)) where
+  empty = Left ([], (0,0,""))
+  (Right a) <|> _ = Right a 
+  (Left  a) <|> (Right b) = Right b
+  (Left (e1, (l1, c1, s1))) <|> (Left (e2, (l2, c2, s2))) = if (l1+c1) > (l2+c2) -- return the error which parsed the most
+                                                            then Left (e1, (l1, c1, s1))
+                                                            else Left (e2, (l2, c2, s2))
+  
+newErr :: String -> Parser String -> Parser String
+newErr newError (Parser oldP) = Parser $ \input -> replace $ oldP input
+  where replace (Right a) = (Right a)
+        replace (Left (oldErr, a)) = (Left (newError:oldErr, a))
+
+charP :: Char -> Parser Char
+charP x = Parser f
+  where
+    f (line, col, (y:ys))
+      | y == x = Right (x, (lineN, colN, ys))
+      | otherwise = Left (("Expected " ++ (show x)):[], (lineN, colN, y:ys))
+      where
+        colN = if isNewLine then 0 else (col+1)
+        lineN = (if isNewLine then 1 else 0) + line
+        isNewLine = y == '\n'
+    f (line, col, []) = Left (("Expected " ++ (show x) ++ ", reached end of file."):[], (line, col, []))
+
+stringP :: String -> Parser String
+stringP input = newErr ("Expected " ++ input) $ sequenceA $ map charP input
