f3bb76766495280e1bb80f3133786d220ca0486c
ameyakakade <ameyakakade@gmail.com>
2026-06-15 14:05:07 +0530

    No need of makefiles
    Compiler creates a .baskellbuild dir and stores all the .as and .o
    files in it. Compiler recompiles itself

--- src/Main.hs
+++ src/Main.hs
@@ -8,19 +8,90 @@ import TargetGasAArch64MacOS
 import Control.Monad
 import System.Environment
 import System.Process
+import System.Directory
 import Data.Maybe
+import Data.List
+import Data.Time.Clock
+
+bd = ".baskellbuild/"
+
+getFileName :: String -> FilePath -> FilePath
+getFileName ext fp = bd ++ (fst $ span (/='.') fp) ++ ext
+
+setDir :: FilePath -> IO FilePath
+setDir fileP = do
+  let (fn, dir) = span (/='/') $ reverse $ fileP
+  if null dir
+  then setCurrentDirectory "."
+  else setCurrentDirectory (reverse dir)
+  return (reverse fn)
 
 main :: IO ()
 main = do
-  (fileName:_) <- getArgs
-  compileFile False fileName
-  fmap show (readProcessWithExitCode "/Users/ameya/.ghcup/bin/ghc" ["Main.hs"] "") >>= putStrLn
-  fmap show (readProcessWithExitCode "/usr/bin/as" ["-arch", "arm64", "-o", "as.o", "as.s"] "") >>= putStrLn
-  fmap show (readProcessWithExitCode "/usr/bin/clang" ["write.o", "as.o"] "") >>= putStrLn
+  args <- getArgs
+
+  let compilerDir = "/Users/ameya/Documents/Programming/baskell/src/"
+  cdir <- getCurrentDirectory
+  setCurrentDirectory compilerDir
+
+  nC <- runIfChanged False (["Main.hs", "Parser.hs", "BParser.hs", "Generator.hs", "TargetGasAArch64MacOS.hs"]) "baskell"
+        (prettyProcess $ readProcessWithExitCode "/Users/ameya/.ghcup/bin/ghc" ["-o", "baskell", "Main.hs"] "")
+
+  let std = compilerDir ++ "write.o"
+
+  if null args then putStrLn "No input files."
+  else do
+    setCurrentDirectory cdir
+    let (fileDirName:_) = args
+    (fileName) <- setDir fileDirName
+    createDirectoryIfMissing False bd
+
+    runIfChanged nC [fileName]
+       (getFileName ".as" fileName)
+       (compileFile False fileName)
+
+    runIfChanged nC [(getFileName ".as" fileName)]
+       (getFileName ".o" fileName)
+       (prettyProcess $ readProcessWithExitCode "/usr/bin/as" ["-arch", "arm64", "-o", (getFileName ".o" fileName), (getFileName ".as" fileName)] "")
+
+    runIfChanged nC [std, (getFileName ".o" fileName)]
+       (fst $ span (/='.') fileName)
+       (prettyProcess $ readProcessWithExitCode "/usr/bin/clang" ["-o", (fst $ span (/='.') fileName), std, (getFileName ".o" fileName)] "")
+    return ()
+
+prettyProcess :: Show a => IO (a, String, String) -> IO ()
+prettyProcess p = do
+  (exit, stdin, stderr) <- p
+  putStrLn $ show exit
+  putStr stdin
+  putStr stderr
+
+runIfChanged :: Show a => Bool -> [FilePath] -> FilePath -> (IO a) -> IO Bool
+runIfChanged force fp out ting = do
+  t <- getCurrentTime
+  cs <- traverse (checkChange out) fp 
+  if (any id cs) || force
+  then do
+    putStrLn $ "Making " ++ out ++ " from " ++ (concat $ intersperse ", " fp)
+    ting
+    putStrLn ""
+    return True
+  else do
+    return False
+
+checkChange :: FilePath -> FilePath -> IO Bool
+checkChange out fp = do
+  inT <- getModificationTime fp
+  fileExists <- doesFileExist out
+  if fileExists
+  then do
+    outT <- getModificationTime out
+    let d = realToFrac $ diffUTCTime inT outT
+    return $ if d > 0 then True else False
+  else return True
 
 compileFile :: Bool -> String -> IO ()
 compileFile dumpInfo fileName = do
-  putStrLn ""
   a <- readFile fileName
   let newLines = map snd $ filter (\(x,_) -> x=='\n') $ zip a [0..]
   let parsed = startParser bProgram a
@@ -39,7 +110,7 @@ compileFile dumpInfo fileName = do
           let asmo = asm (snd irp)
           if null (fst irp)
           then do
-            writeFile "as.s" asmo
+            writeFile (getFileName ".as" fileName) asmo
             putStrLn "Compiled successfully"
           else do
             putStr $ unlines $ map (\e -> if isNothing $ genErrorLocLength e
