83a774f2e42da21e3d12b81d2a8a14b42ba39b10
ameyakakade <ameyakakade@gmail.com>
2026-07-14 00:54:54 +0530

    Baskell is built with profiling info

--- Build.hs
+++ Build.hs
@@ -10,7 +10,7 @@ main = do
     ck <- doesFileExist "src/Main.hs"
     if ck then return () else error "Could not find file \"src/Main.hs\""
     createDirectoryIfMissing False "build/"
-    logProcess $ readProcessWithExitCode "ghc" ["-o", "build/baskell", "-outputdirbuild", "src/Main.hs", "-isrc/"] ""
+    logProcess $ readProcessWithExitCode "ghc" ["-prof", "-fprof-auto", "-rtsopts", "-o", "build/baskell", "-outputdirbuild", "src/Main.hs", "-isrc/"] ""
     runIfChanged False ["src/Test.hs"] "build/btest"
       $ logProcess
       $ readProcessWithExitCode "ghc" ["-o", "build/btest", "-outputdirbuild", "src/Test.hs", "-isrc/"] ""
